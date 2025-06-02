c9b8c8f31d1235a689764b6d0ecac5dd6ef548ef
colt littlehawk <coltonromannose@gmail.com>
2025-06-02 04:08:42 +0000

    added bronze model

--- macros/load_fact.sql
+++ macros/load_fact.sql
@@ -1,6 +1,7 @@
 {% macro load_fact() %}
 COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.WORK_FACTS_COPY
-FROM @MY_S3_STAGE/fact.csv
-FILE_FORMAT = (FORMAT_NAME = 'MY_CSV_FORMAT')
-PURGE = FALSE;
+FROM @{{ var('stage_name') }}/fact.csv
+FILE_FORMAT = (FORMAT_NAME = {{ var('file_format_csv') }})
+PURGE = {{ var('purge_status') }}
+FORCE = TRUE;
 {% endmacro %}
