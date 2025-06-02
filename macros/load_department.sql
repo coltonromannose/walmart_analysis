8e6ff5e5fe2da00a76dc4106ab0e324d31874f36
colt littlehawk <coltonromannose@gmail.com>
2025-06-02 02:26:58 +0000

    macro updates

--- macros/load_department.sql
+++ macros/load_department.sql
@@ -1,12 +1,16 @@
 {% macro load_departments() %}
-COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.departments
+COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.WORK_DEPARTMENTS_COPY
 FROM (
     SELECT
         $1 AS Store,
         $2 AS Dept,
         $3 AS Date,
         $4 AS Weekly_Sales,
-        $5 AS IsHoliday
+        $5 AS IsHoliday,
+        CURRENT_TIMESTAMP() AS INSERT_DTS,
+        CURRENT_TIMESTAMP() AS UPDATE_DTS,
+        metadata$filename AS SOURCE_FILE_NAME,
+        metadata$file_row_number AS SOURCE_FILE_ROW_NUMBER
     FROM @MY_S3_STAGE/department.csv
 )
 FILE_FORMAT = MY_CSV_FORMAT
--- macros/load_fact.sql
+++ macros/load_fact.sql
@@ -1,5 +1,5 @@
 {% macro load_fact() %}
-COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.fact
+COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.WORK_FACTS_COPY
 FROM (
     SELECT
         $1 AS Store,
@@ -13,7 +13,11 @@ FROM (
         $9 AS MarkDown5,
         $10 AS CPI,
         $11 AS Unemployment,
-        $12 AS IsHoliday
+        $12 AS IsHoliday,
+        CURRENT_TIMESTAMP() AS INSERT_DTS,
+        CURRENT_TIMESTAMP() AS UPDATE_DTS,
+        metadata$filename AS SOURCE_FILE_NAME,
+        metadata$file_row_number AS SOURCE_FILE_ROW_NUMBER
     FROM @MY_S3_STAGE/fact.csv
 )
 FILE_FORMAT = MY_CSV_FORMAT
